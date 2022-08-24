5edcf89c7e850684f6b7dc54fc5bb3598b27d786
Victorma <victorma-fire@hotmail.com>
2022-08-24 03:26:16 +0200

    Init Commit

---
+++ Runtime/SimvaPlugin.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFx.Async;
+using Xasu.Auth.Protocols;
+using Xasu.Auth.Protocols.OAuth2;
+using Xasu.Config;
+
+namespace Simva
+{
+
+    public class SimvaPlugin : MonoBehaviour, ISimvaBridge
+    {
+        private SimvaSceneController previousController;
+
+        public void Start()
+        {
+            RunScene("Simva.Login");
+        }
+
+        public void Demo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnAuthUpdated(OAuth2Token token)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RunScene(string name)
+        {
+            DestroyPreviousSimvaScene();
+            var go = SimvaSceneManager.LoadPrefabScene(name);
+            var controller = go.GetComponent<SimvaSceneController>();
+            controller.Render();
+        }
+
+        private void DestroyPreviousSimvaScene()
+        {
+            if (previousController)
+            {
+                previousController.Destroy();
+                previousController = null;
+            }
+        }
+
+        public void StartGameplay()
+        {
+            DestroyPreviousSimvaScene();
+            throw new System.NotImplementedException();
+        }
+
+        public IAsyncOperation StartTracker(TrackerConfig config, IAuthProtocol onlineProtocol, IAuthProtocol backupProtocol)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
