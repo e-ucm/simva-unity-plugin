0737aea4b5e4e80893e65afab0c3cfa50253539f
Victorma <victorma-fire@hotmail.com>
2022-08-25 22:35:38 +0200

    Implemented SimvaPlugin as Standard Bridge

--- Runtime/SimvaPlugin.cs
+++ Runtime/SimvaPlugin.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityFx.Async;
+using Xasu;
 using Xasu.Auth.Protocols;
 using Xasu.Auth.Protocols.OAuth2;
 using Xasu.Config;
@@ -11,21 +13,85 @@ namespace Simva
 
     public class SimvaPlugin : MonoBehaviour, ISimvaBridge
     {
+        public bool SaveAuthUntilCompleted = true;
+        public bool ShowLoginOnStartup = true;
+        public bool ContinueOnQuit = true;
         private SimvaSceneController previousController;
+        private OAuth2Token lastAuth;
 
-        public void Start()
+        public IEnumerator Start()
         {
-            RunScene("Simva.Login");
+            SimvaManager.Instance.Bridge = this;
+
+            Debug.Log("[SIMVA] Starting...");
+            if (SimvaConf.Local == null)
+            {
+                SimvaConf.Local = new SimvaConf();
+                yield return StartCoroutine(SimvaConf.Local.LoadAsync());
+                Debug.Log("[SIMVA] Conf Loaded...");
+            }
+
+            if (!SimvaManager.Instance.IsEnabled)
+            {
+                Debug.Log("[SIMVA] Study is not set! Stopping...");
+                yield break;
+            }
+            else if (SimvaManager.Instance.IsActive)
+            {
+                Debug.Log("[SIMVA] Simva is already started...");
+                // No need to restart
+                yield break;
+            }
+
+            if (ShowLoginOnStartup)
+            {
+                Debug.Log("[SIMVA] Setting current target to Simva.Login...");
+                RunScene("Simva.Login");
+
+                if (PlayerPrefs.HasKey("simva_auth") && SaveAuthUntilCompleted)
+                {
+                    var auth = JsonConvert.DeserializeObject<OAuth2Token>(PlayerPrefs.GetString("simva_auth"));
+                    yield return new WaitForFixedUpdate();
+                    SimvaManager.Instance.LoginWithRefreshToken(auth.RefreshToken);
+                }
+            }
+
+            if (ContinueOnQuit)
+            {
+                Application.wantsToQuit += WantsToQuit;
+            }
+        }
+
+        private void OnApplicationPause(bool paused)
+        {
+            if (paused && lastAuth != null)
+            {
+                PlayerPrefs.SetString("simva_auth", JsonConvert.SerializeObject(lastAuth));
+            }
+        }
+        
+        public bool WantsToQuit()
+        {
+            if (SimvaManager.Instance.IsActive)
+            {
+                SimvaManager.Instance.OnGameFinished();
+                return false;
+            }
+            else
+            {
+                PlayerPrefs.DeleteKey("simva_auth");
+            }
+            return true;
         }
 
         public void Demo()
         {
-            throw new System.NotImplementedException();
+            Debug.Log("Demo!");
         }
 
         public void OnAuthUpdated(OAuth2Token token)
         {
-            throw new System.NotImplementedException();
+            lastAuth = token;
         }
 
         public void RunScene(string name)
@@ -34,6 +100,7 @@ namespace Simva
             var go = SimvaSceneManager.LoadPrefabScene(name);
             var controller = go.GetComponent<SimvaSceneController>();
             controller.Render();
+            previousController = controller;
         }
 
         private void DestroyPreviousSimvaScene()
@@ -48,12 +115,24 @@ namespace Simva
         public void StartGameplay()
         {
             DestroyPreviousSimvaScene();
-            throw new System.NotImplementedException();
+            Debug.Log("Starting Gameplay");
         }
 
         public IAsyncOperation StartTracker(TrackerConfig config, IAuthProtocol onlineProtocol, IAuthProtocol backupProtocol)
         {
-            throw new System.NotImplementedException();
+            Debug.Log("Starting Tracker");
+            var result = new AsyncCompletionSource();
+            XasuTracker.Instance.Init(config, onlineProtocol, backupProtocol)
+                .ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        result.SetException(t.Exception);
+                        return;
+                    }
+                    result.SetCompleted();
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+            return result;
         }
     }
 }
