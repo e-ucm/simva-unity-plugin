eb9a23a82716cd81ad24a882314654f0ea7562dd
Victorma <victorma-fire@hotmail.com>
2022-08-25 22:41:01 +0200

    Added Samples

---
+++ Samples~/Demo/SimvaGameplayTester.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xasu.HighLevel;
+
+namespace Simva
+{
+    public class SimvaGameplayTester : MonoBehaviour
+    {
+        public void SendTrace()
+        {
+            GameObjectTracker.Instance.Interacted("simple_button");
+        }
+
+
+        public void End()
+        {
+            if (Application.isEditor)
+            {
+#if UNITY_EDITOR
+                var wants = ((SimvaPlugin)SimvaManager.Instance.Bridge).WantsToQuit();
+                if (wants)
+                {
+                    UnityEditor.EditorApplication.isPlaying = false;
+                }
+#endif
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
