18d042732f4648eb1ca271219b1e13a2e3aedae5
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2025-06-25 10:45:30 +0200

    fix model for scheduler and activity

--- Runtime/Api/Simva/Model/Schedule.cs
+++ Runtime/Api/Simva/Model/Schedule.cs
@@ -21,9 +21,9 @@ namespace Simva.Model {
     public string Next { get; set; }
 
     /// <summary>
-    /// The next activity ID
+    /// The scheduler url
     /// </summary>
-    /// <value>The next activity ID</value>
+    /// <value>The scheduler url</value>
     [DataMember(Name="url", EmitDefaultValue=false)]
     [JsonProperty(PropertyName = "url")]
     public string Url { get; set; }
@@ -36,16 +36,33 @@ namespace Simva.Model {
     [JsonProperty(PropertyName = "activities")]
     public Dictionary<string, Activity> Activities { get; set; }
 
+    /// <summary>
+    /// Gets or Sets Study
+    /// </summary>
+    [DataMember(Name="study", EmitDefaultValue=false)]
+    [JsonProperty(PropertyName = "study")]
+    public string Study { get; set; }
+
+    /// <summary>
+    /// Gets or Sets Study
+    /// </summary>
+    [DataMember(Name="studyName", EmitDefaultValue=false)]
+    [JsonProperty(PropertyName = "studyName")]
+    public string StudyName { get; set; }
+
 
     /// <summary>
     /// Get the string presentation of the object
     /// </summary>
     /// <returns>String presentation of the object</returns>
-    public override string ToString()  {
+    public override string ToString()
+    {
       var sb = new StringBuilder();
       sb.Append("class InlineResponse200 {\n");
       sb.Append("  Next: ").Append(Next).Append("\n");
       sb.Append("  Url: ").Append(Url).Append("\n");
+      sb.Append("  Study: ").Append(Study).Append("\n");
+      sb.Append("  StudyName: ").Append(StudyName).Append("\n");
       sb.Append("}\n");
       return sb.ToString();
     }
