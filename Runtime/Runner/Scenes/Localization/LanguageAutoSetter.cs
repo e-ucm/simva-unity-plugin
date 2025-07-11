f2ff4df559f485e4af3eaef84f92cc9d2af8cdb9
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2025-07-11 09:36:16 +0200

    fix languageAutoSetter class

---
+++ Runtime/Runner/Scenes/Localization/LanguageAutoSetter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Simva
+{
+    public class LanguageAutoSetter : MonoBehaviour
+    {
+        [SerializeField] private Text textComponent;
+        [SerializeField] private TextMesh textMeshComponent;
+        [SerializeField] private string languageKey;
+
+        private void Awake()
+        {
+            FillName();
+        }
+        void FillName()
+        {
+            var name = LanguageSelectorController.instance.GetName(languageKey);
+            if (textComponent != null)
+            {
+                textComponent.text = name;
+            }
+            else if (textMeshComponent != null)
+            {
+                textMeshComponent.text = name;
+            }
+        }
+    }
+}
